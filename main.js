e9055dd7e50a0e78320acd8fb89d08bf73b8d3db
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2022-10-13 11:26:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function function1() {
+    document.getElementById("audio1").play();
+}
+		function function2() {
+    document.getElementById("audio2").play();
+}
+		function function3() {
+    document.getElementById("audio3").play();
+}
+		function function4() {
+    document.getElementById("audio4").play();
+}
+		function function5() {
+    document.getElementById("audio5").play();
+}
